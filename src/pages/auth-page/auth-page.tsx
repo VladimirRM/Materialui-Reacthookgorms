686d5c062fe4db36620739d088c1ad623c5f6c4a
Vladimir RM <shvdrummer@gmail.com>
2022-10-17 10:42:41 +0300

    npm instal material ui

--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './auth-page.css'
+import Typography from '@mui/material/Typography';
 
  export const AuthPage: React.FC= () => {
   return (
-    <div>auth-page</div>
+    <div>
+          <Typography variant="h1" gutterBottom>
+        h1. Heading
+      </Typography>
+    </div>
   )
 }
 
