caf129e7ea10b217f0771dcd23258fa1f743ce76
Vladimir RM <shvdrummer@gmail.com>
2022-10-17 10:23:14 +0300

    create auth-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { AuthPage } from './pages/auth-page';
 
 
 
 function App() {
   return (
     <div className="App">
-     hello
+     <AuthPage/>
     </div>
   );
 }
---
+++ src/pages/auth-page/auth-page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './auth-page.css'
+
+ export const AuthPage= () => {
+  return (
+    <div>auth-page</div>
+  )
+}
+
+export default AuthPage
