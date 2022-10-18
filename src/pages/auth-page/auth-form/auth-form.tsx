8da8f9ac979a53bccd2388a5273d583ea6018573
Vladimir RM <shvdrummer@gmail.com>
2022-10-18 08:37:10 +0300

    beagin css form

---
+++ src/pages/auth-page/auth-form/auth-form.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./auth-form.css"
+import Typography from '@mui/material/Typography';
+
+ export const AuthForm = () => {
+  return (
+    <div className='auth-form'>
+           <Typography variant="h4" component='div' >
+        h1. Heading
+      </Typography>
+    </div>
+  )
+}
+
+export default AuthForm
--- src/pages/auth-page/auth-page.tsx
+++ src/pages/auth-page/auth-page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './auth-page.css'
 import Typography from '@mui/material/Typography';
+import { AuthForm } from './auth-form';
 
  export const AuthPage: React.FC= () => {
   return (
