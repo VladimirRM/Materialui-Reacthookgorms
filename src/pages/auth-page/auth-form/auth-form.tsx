3f41298ce698e7dee537493cf9e1da775da90fdb
Vladimir RM <shvdrummer@gmail.com>
2022-10-18 09:50:44 +0300

    create form & text-field

--- src/pages/auth-page/auth-form/auth-form.tsx
+++ src/pages/auth-page/auth-form/auth-form.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./auth-form.css"
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
  export const AuthForm = () => {
   return (
@@ -11,6 +12,14 @@ import Typography from '@mui/material/Typography';
            <Typography variant="subtitle1" component='div' gutterBottom={true} className='auth-form__subtitle1' >
             чтобы получить доступ
       </Typography>
+      <form>
+      <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+        />
+      </form>
     </div>
   )
 }
